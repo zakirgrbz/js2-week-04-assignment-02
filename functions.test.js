465b11f8f3e5978776f46aac71d60e706b6c74f5
huseyintkt <63949581+huseyintkt@users.noreply.github.com>
2022-11-29 10:39:11 +0100

    Updated

--- functions.test.js
+++ functions.test.js
@@ -24,7 +24,7 @@ test('According to the given parameter, the result should be abcg,efd', () => {
 test('According to the given parameter, the result should be not possible', () => {
   let result = isTheWordSplit([
     'hicoders',
-    'hi,code,hicoders,coder,hicode,hicoder',
+    'code,coder,hi,hicode,hicoder,hicoders',
   ]);
   expect(result).toBe('not possible');
 });
