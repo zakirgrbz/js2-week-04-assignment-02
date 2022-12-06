f88552b7c7c6606e529b1ec12aac87d4c893d19b
Zakir Gürbüz <110854949+zakirgrbz@users.noreply.github.com>
2022-12-06 01:45:55 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,3 +1,15 @@
-const isTheWordSplit = () => {};
-
-export { isTheWordSplit };
+const isTheWordSplit = (zArray) => {
+    let word = zArray[0];
+    let array = zArray[1].split(",");
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array.length; j++) {
+        if (array[i] + array[j] == word) {
+          return array[i] + "," + array[j];
+        }
+      }
+    }
+    return "not possible";
+  };
+  
+  export { isTheWordSplit };
+  
